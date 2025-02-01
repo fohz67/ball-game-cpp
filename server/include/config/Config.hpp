f554c10790250aa7a156418ed236734c8b06bc76
fohz67 <benjamin.gayaud@epitech.eu>
2025-02-01 16:28:14 +0100

    add: pellets

--- server/include/config/Config.hpp
+++ server/include/config/Config.hpp
@@ -14,15 +14,15 @@ struct Config {
 
     struct GameMode {
         // World
-        static constexpr uint16_t WORLD_SIZE = 500.0f;
+        static constexpr uint16_t WORLD_SIZE = 1000.0f;
 
         // Cell
-        static constexpr double SPAWN_MASS = 2000.0f;
+        static constexpr double SPAWN_MASS = 4000.0f;
         static constexpr double CELL_SPEED = 0.5;
         static constexpr double DECREASE_SPEED_THRESHOLD = 0.05;
 
         // Pellet
-        static constexpr int PELLET_COUNT = 300;
-        static constexpr double PELLET_MASS = 200.0f;
+        static constexpr int PELLET_COUNT = 100;
+        static constexpr double PELLET_MASS = 20.0f;
     };
 };
