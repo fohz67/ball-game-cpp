1e34e4651b6f0d56e94196aea5e36e7d244b12bf
fohz67 <benjamin.gayaud@epitech.eu>
2025-02-02 02:37:18 +0100

    add: scripts

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 git submodule update --init --recursive
+
 cmake -B build .
 cd build/
 make
--- scripts/clang.sh
+++ scripts/clang.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
+
 find "server" \( -name "*.cpp" -o -name "*.hpp" \) -exec clang-format -i {} +
 find "client" \( -name "*.cpp" -o -name "*.hpp" \) -exec clang-format -i {} +
 find "gengine" \( -name "*.cpp" -o -name "*.hpp" \) -exec clang-format -i {} +
